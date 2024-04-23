4bd72dec5ebad55235b0953f12c3f5143a9ef267
Fabián Huertas <miloh9318@hotmail.com>
2024-04-22 20:17:04 -0500

    validando el formulario

--- js/app.js
+++ js/app.js
@@ -8,6 +8,8 @@ const gastoListado = document.querySelector('#gastos ul');
 eventListeners();
 function eventListeners() {
     document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
+
+    formulario.addEventListener('submit', agregarGasto);
 }
 
 // Clases
@@ -28,6 +30,33 @@ class UI {
         // Agregar al HTML
         document.querySelector('#total').textContent = presupuesto;
         document.querySelector('#restante').textContent = restante;
+    }
+
+    imprimirAlerta(mensaje, tipo) {
+        // crear el div
+
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert');
+
+        if (tipo === 'error') {
+            divMensaje.classList.add('alert-danger');
+        } else {
+            divMensaje.classList.add('alert-success');
+        }
+
+        // Mensaje de error
+
+        divMensaje.textContent = mensaje;
+
+        // Insertar en el HTML 
+
+        document.querySelector('.primario').insertBefore(divMensaje, formulario);
+
+        // Quitar el HTML
+
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 3000);
     }
 }
 
@@ -54,3 +83,26 @@ function preguntarPresupuesto() {
 
     ui.insertarPresupuesto(presupuesto);
 }
+
+    // Añade gastos
+
+    function agregarGasto(e) {
+        e.preventDefault();
+
+        // Leer los datos del formulario
+
+        const nombre = document.querySelector('#gasto').value;
+        const cantidad = document.querySelector('#cantidad').value;
+
+        // Validar
+
+        if(nombre === '' || cantidad === '') {
+            ui.imprimirAlerta('Ambos campos son obligatorios', 'error');
+            return;
+        } else if (cantidad <= 0 || isNaN(cantidad)) {
+            ui.imprimirAlerta('Cantidad no valida', 'error');
+            return;
+        }
+
+        console.log('Agregando gasto...');
+    }
